97c59918396e941090eeca88f5349e105247403f
Ik0n <Ik0nChannel1@mail.ru>
2022-04-06 22:51:46 +0300

    Add delete Note Add date field for Note UPD FIXED DatePicker. Add DatePickerDialog

--- app/src/main/java/com/example/note/ui/EditNoteFragment.java
+++ app/src/main/java/com/example/note/ui/EditNoteFragment.java
@@ -1,9 +1,11 @@
 package com.example.note.ui;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +18,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
 
+import com.example.note.DatePickerDialog;
 import com.example.note.R;
 import com.example.note.data.Note;
 
@@ -27,31 +31,23 @@ import java.util.Date;
 public class EditNoteFragment extends Fragment {
 
     private Note note;
-
     private EditText title;
     private EditText description;
     private EditText date;
-
     private Button save;
-
     private FragmentManager manager;
-
     private Date formatDate;
-
-    private AlertDialog.Builder dialog = null;
 
     public interface Controller {
         void saveButtonPressed(Note note);
     }
 
     public static EditNoteFragment getInstance(Note note) {
-
         EditNoteFragment fragment = new EditNoteFragment();
         Bundle args = new Bundle();
         args.putSerializable(Note.NOTE, note);
         fragment.setArguments(args);
         return fragment;
-
     }
 
     @Nullable
@@ -62,42 +58,28 @@ public class EditNoteFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
         title = view.findViewById(R.id.edit_note_title);
         description = view.findViewById(R.id.edit_note_description);
         date = view.findViewById(R.id.edit_note_date);
         save = view.findViewById(R.id.edit_button_save);
+        getParentFragmentManager().setFragmentResultListener(DatePickerDialog.DATE_PICKER_DIALOG, this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                formatDate = (Date) result.getSerializable(DatePickerDialog.DATE_PICKER_DIALOG);
+                date.setText(new SimpleDateFormat("dd.MM.yy").format(formatDate));
 
-        View dialogView = getLayoutInflater().inflate(R.layout.dialog_datepicker, (ViewGroup) view, false);
-        DatePicker datePicker = dialogView.findViewById(R.id.date_picker);
+            }
+        });
         date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (dialog == null) {
-                    dialog = new AlertDialog.Builder(view.getContext())
-                            .setTitle("Chose date")
-                            .setView(dialogView)
-                            .setCancelable(true)
-                            .setPositiveButton("Chose", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    formatDate = new Date(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth());
-                                    date.setText(new SimpleDateFormat("dd.MM.yy").format(formatDate));
-                                }
-                            })
-                            .setNeutralButton("Cancel", null);
-                }
-
-                dialog.show();
+                showDatePickerDialog();
             }
         });
-
         if (getArguments() != null) {
             note = (Note) getArguments().getSerializable(Note.NOTE);
         }
-
         manager = requireActivity().getSupportFragmentManager();
-
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -110,31 +92,28 @@ public class EditNoteFragment extends Fragment {
                 } else {
                     note = new Note(title.getText().toString(), description.getText().toString(), formatDate);
                 }
-
                 if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
                     manager
                             .beginTransaction()
                             .replace(R.id.landscape_notes_list_fragment_holder, NotesListFragment.getInstance(note))
                             .commit();
-
                     manager
                             .beginTransaction()
                             .replace(R.id.landscape_notes_edit_fragment_holder, new Fragment())
                             .commit();
-
                 } else {
-
                     manager
                             .beginTransaction()
                             .replace(R.id.portrait_fragment_holder, NotesListFragment.getInstance(note))
                             .commit();
                 }
-
             }
         });
-
         init(note);
+    }
 
+    private void showDatePickerDialog() {
+        new DatePickerDialog().show(getParentFragmentManager(), DatePickerDialog.DATE_PICKER_DIALOG);
     }
 
     public void passMessage(Note note){
@@ -142,34 +121,22 @@ public class EditNoteFragment extends Fragment {
     }
 
     private void init(Note note) {
-
         Bundle args = getArguments();
-
         if (args != null) {
-
             if (note != null) {
-
                 note = (Note) args.getSerializable(Note.NOTE);
-
             } else {
-
                 args.putSerializable(Note.NOTE, note);
                 setArguments(args);
             }
-
             title.setText(note.getTitle());
             description.setText(note.getDescription());
             date.setText(new SimpleDateFormat("dd.MM.yy").format(note.getDate()));
-
         } else {
-
             if (note != null) {
                 args.putSerializable(Note.NOTE, note);
                 setArguments(args);
             }
-
         }
-
     }
-
 }
